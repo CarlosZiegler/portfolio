3d42327750e8184c06311043e1dfa8c9ab5a4923
carlosziegler <carlos.ziegler@gmail.com>
2020-04-21 12:27:42 +0200

    Fibonacci Route

---
+++ api/fibonacci/[num].js
@@ -0,0 +1,38 @@
+const Cors = require('micro-cors')
+
+const cors = Cors({
+    allowedMethods: ['GET']
+})
+
+async function Test(req, res) {
+
+    const { num } = req.query
+
+    const fibonacci = () => {
+
+        let fibonacciArray = []
+        let element = 0
+
+        for (let index = 0; element < 350; index++) {
+
+            if (index < 2) {
+                fibonacciArray.push(index)
+                continue
+            }
+
+            element = fibonacciArray[(index - 1)] + fibonacciArray[(index - 2)]
+            fibonacciArray.push(element)
+
+        }
+        return fibonacciArray
+    }
+
+    const isFibonnaci = (num) => {
+        return fibonacci().includes(num)
+    }
+
+
+    return res.json({ num })
+}
+
+module.exports = cors(Test)
