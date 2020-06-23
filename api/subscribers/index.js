1a5af2254897238f0cd6d3606f00bd381b4c477e
carlosziegler <carlos.ziegler@gmail.com>
2020-06-23 14:41:43 +0200

    get subscribers from filipe with cherrios

--- api/subscribers/index.js
+++ api/subscribers/index.js
@@ -1,29 +1,39 @@
 const puppeteer = require('puppeteer')
 const Cors = require('micro-cors')
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+
+const siteUrl = "https://www.youtube.com/channel/UCU5JicSrEM5A63jkJ2QvGYw";
+
+const fetchData = async () => {
+    const result = await axios.get(siteUrl);
+    return cheerio.load(result.data);
+};
 
 const cors = Cors({
     allowedMethods: ['GET']
 })
 
-async function getCountChannel(url) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
-    let data = await page.evaluate(() => {
-        const [element] = document.querySelector('#subscriber-count').innerHTML.split(' ')
-        return element;
-    });
+// async function getCountChannel(url) {
+//     const browser = await puppeteer.launch()
+//     const page = await browser.newPage()
+//     await page.goto(url)
+//     let data = await page.evaluate(() => {
+//         const [element] = document.querySelector('#subscriber-count').innerHTML.split(' ')
+//         return element;
+//     });
 
-    await browser.close()
-    console.log(data)
-    return data
+//     await browser.close()
+//     console.log(data)
+//     return data
 
-}
+// }
 
 
 async function Subscribers(req, res) {
 
-    const count = getCountChannel('https://www.youtube.com/channel/UCU5JicSrEM5A63jkJ2QvGYw')
+    const count = fetchData()
 
     return res.json({
         count
