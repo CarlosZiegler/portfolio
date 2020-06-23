67128f20226d0075e75c380b73303762636829bb
carlosziegler <carlos.ziegler@gmail.com>
2020-06-23 15:08:37 +0200

    get subscribers from filipe cherio

--- api/subscribers/index.js
+++ api/subscribers/index.js
@@ -1,4 +1,3 @@
-const puppeteer = require('puppeteer')
 const Cors = require('micro-cors')
 const axios = require("axios");
 const cheerio = require("cheerio");
@@ -13,7 +12,7 @@ const fetchData = async () => {
 
 const getResults = async () => {
     const $ = await fetchData();
-    const substr = $('#subscriber-count').text();
+    const substr = $('yt-formatted-string[id="subscriber-count"]').text();
     console.log(substr)
     return substr
 }
@@ -22,25 +21,9 @@ const cors = Cors({
     allowedMethods: ['GET']
 })
 
-async function getCountChannel(url) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
-    let data = await page.evaluate(() => {
-        const [element] = document.querySelector('#subscriber-count').innerHTML.split(' ')
-        return element;
-    });
-
-    await browser.close()
-    console.log(data)
-    return data
-
-}
-
-
 async function Subscribers(req, res) {
 
-    const count = await getCountChannel()
+    const count = await getResults()
 
     return res.json({
         count
