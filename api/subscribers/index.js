a477c7c8a4f12c9e43a6bf392dcb2dbfcbebf8d8
carlosziegler <carlos.ziegler@gmail.com>
2020-06-23 14:58:38 +0200

    get subscribers from filipe puppeteer

--- api/subscribers/index.js
+++ api/subscribers/index.js
@@ -22,25 +22,25 @@ const cors = Cors({
     allowedMethods: ['GET']
 })
 
-// async function getCountChannel(url) {
-//     const browser = await puppeteer.launch()
-//     const page = await browser.newPage()
-//     await page.goto(url)
-//     let data = await page.evaluate(() => {
-//         const [element] = document.querySelector('#subscriber-count').innerHTML.split(' ')
-//         return element;
-//     });
+async function getCountChannel(url) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+    let data = await page.evaluate(() => {
+        const [element] = document.querySelector('#subscriber-count').innerHTML.split(' ')
+        return element;
+    });
 
-//     await browser.close()
-//     console.log(data)
-//     return data
+    await browser.close()
+    console.log(data)
+    return data
 
-// }
+}
 
 
 async function Subscribers(req, res) {
 
-    const count = getResults()
+    const count = await getCountChannel()
 
     return res.json({
         count
